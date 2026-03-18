fa7ffe352c7cfeba13015300959b0efabb5d5cc1
ProjectRathish <rathish@example.com>
2026-03-18 17:15:43 +0530

    feat: move and update database schema and seeds for easy project cloning

---
+++ database/schema.sql
@@ -0,0 +1,175 @@
+CREATE TABLE `candidates` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `school_id` int(11) NOT NULL,
+  `election_id` int(11) NOT NULL,
+  `voter_id` int(11) NOT NULL,
+  `post_id` int(11) NOT NULL,
+  `photo` varchar(255) DEFAULT NULL,
+  `symbol` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `voter_id` (`voter_id`,`election_id`),
+  KEY `post_id` (`post_id`),
+  KEY `school_id` (`school_id`),
+  KEY `election_id` (`election_id`),
+  CONSTRAINT `candidates_ibfk_1` FOREIGN KEY (`voter_id`) REFERENCES `voters` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `candidates_ibfk_2` FOREIGN KEY (`post_id`) REFERENCES `posts` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `candidates_ibfk_3` FOREIGN KEY (`school_id`) REFERENCES `schools` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `candidates_ibfk_4` FOREIGN KEY (`election_id`) REFERENCES `elections` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `classes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `school_id` int(11) NOT NULL,
+  `election_id` int(11) NOT NULL,
+  `section_id` int(11) NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  KEY `school_id` (`school_id`),
+  KEY `election_id` (`election_id`),
+  KEY `section_id` (`section_id`),
+  CONSTRAINT `classes_ibfk_1` FOREIGN KEY (`school_id`) REFERENCES `schools` (`id`),
+  CONSTRAINT `classes_ibfk_2` FOREIGN KEY (`election_id`) REFERENCES `elections` (`id`),
+  CONSTRAINT `classes_ibfk_3` FOREIGN KEY (`section_id`) REFERENCES `sections` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `elections` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `school_id` int(11) DEFAULT NULL,
+  `name` varchar(200) DEFAULT NULL,
+  `start_time` datetime DEFAULT NULL,
+  `end_time` datetime DEFAULT NULL,
+  `status` enum('DRAFT','CONFIGURING','READY','ACTIVE','PAUSED','CLOSED') DEFAULT 'DRAFT',
+  `created_by` int(11) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  KEY `school_id` (`school_id`),
+  KEY `fk_election_creator` (`created_by`),
+  CONSTRAINT `elections_ibfk_1` FOREIGN KEY (`school_id`) REFERENCES `schools` (`id`),
+  CONSTRAINT `fk_election_creator` FOREIGN KEY (`created_by`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `polling_booths` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `school_id` int(11) NOT NULL,
+  `election_id` int(11) NOT NULL,
+  `booth_number` varchar(50) NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `capacity` int(11) DEFAULT NULL,
+  `status` varchar(50) DEFAULT 'ACTIVE',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `election_id` (`election_id`,`booth_number`),
+  KEY `school_id` (`school_id`),
+  CONSTRAINT `polling_booths_ibfk_1` FOREIGN KEY (`school_id`) REFERENCES `schools` (`id`),
+  CONSTRAINT `polling_booths_ibfk_2` FOREIGN KEY (`election_id`) REFERENCES `elections` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `posts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `school_id` int(11) NOT NULL,
+  `election_id` int(11) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `candidate_classes` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`candidate_classes`)),
+  `voting_classes` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL CHECK (json_valid(`voting_classes`)),
+  `gender_rule` enum('ANY','M','F') DEFAULT 'ANY',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `school_id` (`school_id`,`election_id`,`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `school_enquiries` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `school_name` varchar(200) NOT NULL,
+  `contact_person` varchar(150) NOT NULL,
+  `contact_email` varchar(150) NOT NULL,
+  `contact_phone` varchar(20) NOT NULL,
+  `location` varchar(150) DEFAULT NULL,
+  `message` text DEFAULT NULL,
+  `status` enum('PENDING','APPROVED','REJECTED') DEFAULT 'PENDING',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `schools` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL,
+  `code` varchar(50) NOT NULL,
+  `location` varchar(150) DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `code` (`code`),
+  UNIQUE KEY `code_2` (`code`)
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `sections` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `school_id` int(11) NOT NULL,
+  `election_id` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_section` (`school_id`,`election_id`,`name`),
+  KEY `fk_sections_election` (`election_id`),
+  CONSTRAINT `fk_sections_election` FOREIGN KEY (`election_id`) REFERENCES `elections` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `fk_sections_school` FOREIGN KEY (`school_id`) REFERENCES `schools` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `school_id` int(11) DEFAULT NULL,
+  `username` varchar(100) NOT NULL,
+  `password_hash` varchar(255) NOT NULL,
+  `role` enum('SUPER_ADMIN','SCHOOL_ADMIN','BOOTH_OFFICER') NOT NULL,
+  `must_change_password` tinyint(1) DEFAULT 1,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  KEY `school_id` (`school_id`),
+  CONSTRAINT `users_ibfk_1` FOREIGN KEY (`school_id`) REFERENCES `schools` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `voters` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `school_id` int(11) NOT NULL,
+  `election_id` int(11) NOT NULL,
+  `admission_no` varchar(50) NOT NULL,
+  `name` varchar(200) NOT NULL,
+  `class_id` int(11) NOT NULL,
+  `sex` enum('M','F') NOT NULL,
+  `is_active` tinyint(1) DEFAULT 0,
+  `has_voted` tinyint(1) DEFAULT 0,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `election_id` (`election_id`,`admission_no`),
+  KEY `school_id` (`school_id`),
+  KEY `class_id` (`class_id`),
+  CONSTRAINT `voters_ibfk_1` FOREIGN KEY (`school_id`) REFERENCES `schools` (`id`),
+  CONSTRAINT `voters_ibfk_2` FOREIGN KEY (`election_id`) REFERENCES `elections` (`id`),
+  CONSTRAINT `voters_ibfk_3` FOREIGN KEY (`class_id`) REFERENCES `classes` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+CREATE TABLE `voting_machines` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `school_id` int(11) NOT NULL,
+  `election_id` int(11) NOT NULL,
+  `booth_id` int(11) NOT NULL,
+  `machine_name` varchar(100) NOT NULL,
+  `machine_code` varchar(50) NOT NULL,
+  `machine_token` varchar(255) NOT NULL,
+  `status` enum('FREE','BUSY','OFFLINE') DEFAULT 'FREE',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `machine_code` (`machine_code`),
+  UNIQUE KEY `machine_token` (`machine_token`),
+  UNIQUE KEY `unique_machine_name_per_booth` (`booth_id`,`machine_name`),
+  KEY `school_id` (`school_id`),
+  KEY `election_id` (`election_id`),
+  CONSTRAINT `voting_machines_ibfk_1` FOREIGN KEY (`school_id`) REFERENCES `schools` (`id`),
+  CONSTRAINT `voting_machines_ibfk_2` FOREIGN KEY (`election_id`) REFERENCES `elections` (`id`),
+  CONSTRAINT `voting_machines_ibfk_3` FOREIGN KEY (`booth_id`) REFERENCES `polling_booths` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
